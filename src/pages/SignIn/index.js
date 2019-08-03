f47562067bc0295914022c81cd9a90cc97009d2f
Vitor Pacheco <vpacheco.costa@gmail.com>
2019-08-03 14:14:15 -0300

    Pagina de login

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {Link, withRouter} from 'react-router-dom';
+
+import api from '../../services/api';
+import {login} from '../../services/auth';
+
+import { Form, Container } from "./styles";
+
+class SignIn extends Component {
+    state = {
+        email: '',
+        password: '',
+        error: ''
+    };
+
+    handleSignIn = async e => {
+        e.preventDefault();
+
+        const {email, password} = this.state;
+
+        if (!email || !password) {
+            this.setState({error: 'Preencha e-mail e senha para continuar'});
+        } else {
+            try {
+                const response = await api.post('/login', {email, password});
+                login(response.data.token);
+                this.props.history.push('/app');
+            } catch (err) {
+                this.setState({error: 'Houve um problema com o login, verifique suas credenciais.'})
+            }
+        }
+    };
+
+    render() {
+        return (
+            <Container>
+                <Form onSubmit={this.handleSignIn}>
+                    {this.state.error && <p>{this.state.error}</p>}
+
+                    <input type="email"
+                           placeholder="Endereço de e-mail"
+                           onChange={e => this.setState({email: e.target.value})}/>
+
+                    <input type="password"
+                           placeholder="Senha"
+                           onChange={e => this.setState({password: e.target.value})}/>
+
+                    <button type="submit">Entrar</button>
+                    <hr/>
+                    <Link to="/signup">Cadastrar</Link>
+                </Form>
+            </Container>
+        );
+    }
+}
+
+export default withRouter(SignIn)
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
 import {isAuthenticated} from './services/auth';
 import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 const PrivateRoute = ({component: Component, ...rest}) => (
     <Route
@@ -16,7 +17,7 @@ const PrivateRoute = ({component: Component, ...rest}) => (
 const Routes = () => (
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={() => <h1>Login</h1>}/>
+            <Route exact path="/" component={SignIn}/>
             <Route path="/signup" component={SignUp}/>
             <PrivateRoute path="/app" component={() => <h1>App</h1>}/>
             <Route path="*" component={() => <h1>404</h1>}/>
